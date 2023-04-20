e4903710c2e8a61596e5c37335d0bf70bf2311de
minho-spotu <minho@spotu.io>
2023-04-20 11:44:43 +0900

    feat(style): add color_scheme.dart

---
+++ lib/style_config/color_scheme.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+
+const seed = Color(0xFF00E338);
+
+ColorScheme colorScheme = lightColorScheme;
+
+const lightColorScheme = ColorScheme(
+  brightness: Brightness.light,
+  primary: Color(0xfffcca00),
+  onPrimary: Color(0xfffaa719),
+  primaryContainer: Color(0xfffff5cc),
+  onPrimaryContainer: Color(0xff4E6494),
+  secondary: Color(0xff89d123),
+  onSecondary: Color(0xfface161),
+  secondaryContainer: Color(0xffb4e470),
+  onSecondaryContainer: Color(0xff96d936),
+  tertiary: Color(0xff71d8ff),
+  onTertiary: Color(0xff81e0d6),
+  tertiaryContainer: Color(0xff2ac3ff),
+  onTertiaryContainer: Color(0xff2a9bfc),
+  error: Color(0xffff0c00),
+  onError: Color(0xfff84443),
+  errorContainer: Color(0xffffdad4),
+  onErrorContainer: Color(0xff410001),
+  background: Color(0xffffffff),
+  onBackground: Color(0xffededed),
+  surface: Color(0xff9e9e9e),
+  onSurface: Color(0xffb2b2b2),
+  surfaceVariant: Color(0xff929191),
+  onSurfaceVariant: Color(0xff5a5a5a),
+  outline: Color(0xffe0e0e0),
+  shadow: Color(0xff000000),
+  inversePrimary: Color(0xffe5fc4a),
+  inverseSurface: Color(0xff7f54c9),
+  surfaceTint: Color(0xffDCDFBE),
+  onInverseSurface: Color(0xffffffff),
+  outlineVariant: Color(0xffffffff),
+  secondaryVariant: Color(0xffffffff),
+  primaryVariant: Color(0x00068cc3),
+);
+
+const darkColorScheme = ColorScheme(
+  brightness: Brightness.dark,
+  primary: Color(0xFF0BE53A),
+  onPrimary: Color(0xFF003A04),
+  primaryContainer: Color(0xFF005308),
+  onPrimaryContainer: Color(0xFF6CFF71),
+  secondary: Color(0xFFBACBB3),
+  onSecondary: Color(0xFF253422),
+  secondaryContainer: Color(0xFF3C4B38),
+  onSecondaryContainer: Color(0xFFD6E8CE),
+  tertiary: Color(0xFFA0CFD4),
+  onTertiary: Color(0xFF00363A),
+  tertiaryContainer: Color(0xFF1E4D51),
+  onTertiaryContainer: Color(0xFFBCEBF0),
+  error: Color(0xFFFFB4A9),
+  errorContainer: Color(0xFF930006),
+  onError: Color(0xFF680003),
+  onErrorContainer: Color(0xFFFFDAD4),
+  background: Color(0xFF1A1C19),
+  onBackground: Color(0xFFE2E3DC),
+  surface: Color(0xFF1A1C19),
+  onSurface: Color(0xFFE2E3DC),
+  surfaceVariant: Color(0xFF42493F),
+  onSurfaceVariant: Color(0xFFC2C8BC),
+  outline: Color(0xFF8C9288),
+  onInverseSurface: Color(0xFF1A1C19),
+  inverseSurface: Color(0xff2F312D),
+  inversePrimary: Color(0xFF006E0F),
+  shadow: Color(0xFF000000),
+);
+
+const primary = Color(0xff0047ab);
+const onPrimary = Color(0xffe5fc4a);
+const primaryContainer = Color(0xff497bc2);
+const onPrimaryContainer = Color(0xffffffff);
+const secondary = Color(0xff003175);
+const onSecondary = Color(0xffffffff);
+const secondaryContainer = Color(0xffe2e3dc);
+const onSecondaryContainer = Color(0xff303030);
+const tertiary = Color(0xffe5fc4a);
+const onTertiary = Color(0xff0047ab);
+const tertiaryContainer = Color(0xffe2e3dc);
+const onTertiaryContainer = Color(0xff068cc3);
+const error = Color(0xffba1b1b);
+const onError = Color(0xfffffdfd);
+const errorContainer = Color(0xffffdad4);
+const onErrorContainer = Color(0xff410001);
+const background = Color(0xfff1f1f1);
+const onBackground = Color(0xff303030);
+const surface = Color(0xffffffff);
+const onSurface = Color(0xff939393);
+const surfaceVariant = Color(0xfffeeb34);
+const onSurfaceVariant = Color(0xff000000);
+const outline = Color(0xffe4e4e4);
+const shadow = Color(0xff000000);
+const inversePrimary = Color(0xffe5fc4a);
+const inverseSurface = Color(0xff232323);
+const onInverseSurface = Color(0xffffffff);
+const chartLabelColor = Color(0xff1a1c19);
+const primaryVariant = Color(0x00068cc3);
