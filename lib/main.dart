5d6c49c8b6c5bddcf0c7041716821a4f433e3a7f
jw <jw@spotu.io>
2023-05-03 15:33:29 +0900

    test commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // test text for test commit
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
